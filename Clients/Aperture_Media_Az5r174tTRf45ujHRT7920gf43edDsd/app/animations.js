32628af221250e561488fb831fdde189c3347d5f
Luis Rivera <122839916+Lerrgo@users.noreply.github.com>
2026-03-09 21:58:11 -0500

    Adding images and final changes

--- Clients/Aperture_Media_Az5r174tTRf45ujHRT7920gf43edDsd/app/animations.js
+++ Clients/Aperture_Media_Az5r174tTRf45ujHRT7920gf43edDsd/app/animations.js
@@ -3,8 +3,7 @@ const observer = new IntersectionObserver(
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add("active");
-            } else {
-                entry.target.classList.remove("active");
+                observer.unobserve(entry.target);
             }
         });
     },
